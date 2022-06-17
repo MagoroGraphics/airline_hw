6d6d19b231160d192affb6cea00655ecc78407e3
Mario <magoro.graphics@gmail.com>
2022-06-17 15:24:45 +0100

    Has Plane Class

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,15 @@
+public enum PlaneType {
+
+    BOEING747(524, 103419),
+    AIRBUS_A320(1473, 203957),
+    BOEING777(657, 125876),
+    AIRBUS_A330(1865,208856);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    PlaneType(int capacity, int totalWeight) {
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+}
